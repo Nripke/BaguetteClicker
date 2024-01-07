28ba89029228696923093271ba33b5e0555f31b3
Nripke <86622900+Nripke@users.noreply.github.com>
2024-01-07 16:03:04 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var baguettes = 0;
 var epicbaguettes = 0;
 var bakeryUnlocked = false;
+
 var bakeries = 0;
 var bakeryCM = 1.1;
+
+var markets = 0;
+var marketCM = 1.13;
 
 /*
 PROGRESSION:
 
 Bakery
-
+Market
 Factory
 
 
@@ -42,9 +46,12 @@ function updateBaguetteCounters()
     if (document.getElementById("bakery-count") != null) {document.getElementById("bakery-count").textContent = bakeries;}
     if (document.getElementById("bakery-production") != null) {document.getElementById("bakery-production").textContent = bakeries*1;}
 
+    if (document.getElementById("market-count") != null) {document.getElementById("market-count").textContent = markets;}
+    if (document.getElementById("market-production") != null) {document.getElementById("market-production").textContent = markets*10;}
+
     //Update Automated Costs
     if (document.getElementById("bakery-cost") != null) {document.getElementById("bakery-cost").textContent = Math.floor(100*Math.pow(bakeryCM, bakeries));}
-
+    if (document.getElementById("market-cost") != null) {document.getElementById("market-cost").textContent = Math.floor(1000*Math.pow(marketCM, markets));}
 }
 
 function goFurnace()
@@ -109,7 +116,20 @@ function buyBakery()
     }
 }
 
-
+function buyMarket()
+{
+    var cost = Math.floor(1000*Math.pow(marketCM, markets));
+    if (baguettes >= cost)
+    {
+        baguettes -= cost;
+        markets += 1;
+        playAnimation(document.getElementById("buy-market-button"), "marketClick");
+        updateBaguetteCounters();
+    } else {
+        //Play 'cannot afford' animation
+        playAnimation(document.getElementById("buy-market-title"), "cantPurchase");
+    }
+}
 
 function save()
 {
@@ -118,7 +138,8 @@ function save()
         baguettes: baguettes,
         epicbaguettes: epicbaguettes,
         bakeryUnlocked: bakeryUnlocked,
-        bakeries: bakeries
+        bakeries: bakeries,
+        markets: markets
     }
 
     localStorage.setItem("save", JSON.stringify(save));
@@ -133,15 +154,17 @@ function load()
     if (typeof savedata.epicbaguettes !== "undefined") {epicbaguettes = savedata.epicbaguettes;}else {epicbaguettes = 0;}
     if (typeof savedata.bakeryUnlocked !== "undefined") {bakeryUnlocked = savedata.bakeryUnlocked;}else {bakeryUnlocked = false;}
     if (typeof savedata.bakeries !== "undefined") {bakeries = savedata.bakeries;}else {bakeries = 0;}
+    if (typeof savedata.markets !== "undefined") {markets = savedata.markets;}else {markets = 0;}
 }
 
 
 
 
 
-//Bakery Generation
-setInterval(function bakeryGenerate() {
+//Auto Generation
+setInterval(function generateBaguettes() {
     baguettes += 1*bakeries;
+    baguettes += 10*markets;
     updateBaguetteCounters();
 }, 1000);
 
