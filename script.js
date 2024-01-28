d832b92eb2a3c6a68a6886c8a0ae489e9dc8117f
Nripke <86622900+Nripke@users.noreply.github.com>
2024-01-27 22:06:40 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -707,7 +707,7 @@ function load()
     if (typeof savedata.timePlayed !== "undefined") {timePlayed = savedata.timePlayed;}else {timePlayed = 0;}
     if (typeof savedata.clicks !== "undefined") {clicks = savedata.clicks;}else {clicks = 0;}
     if (typeof savedata.blackholes !== "undefined") {blackholes = savedata.blackholes;}else {blackholes = 0;}
-    if (typeof savedata.researchClickUpgrade !== "undefined") {researchClickUpgrade = savedata.researchClickUpgrade;}else {researchClickUpgrade = 0;}
+    if (typeof savedata.researchClickUpgrade !== "undefined") {researchClickUpgrade = savedata.researchClickUpgrade;}else {researchClickUpgrade = 1;}
 }
 
 function reset()
